670805b828491442bddefbcdd90a86437af90b4f
nico <macharenicolas@gmail.com>
2019-04-13 21:12:18 -0300

    jade

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+//imports iniciales
 var express = require("express");
 var app = express();
 
+app.set("view engine", "jade");
+
+
+//urls
 app.get("/", function(req, res){
-    res.send("hola mundo");
+    res.render("index");
 });
 
+//listen final
 app.listen(8080, function(){
     console.log("Inicio...");
 });
