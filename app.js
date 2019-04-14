4090a056308c0155b08f4b043d8060e6cf05883d
nico <macharenicolas@gmail.com>
2019-04-13 21:25:09 -0300

    body parser 1

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 //imports iniciales
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine", "jade");
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //urls
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.get("/creations", function(req, res){
+    res.render("newusers");
+});
+
+app.post("/inicioUsers", function(req, res){
+    console.log("nombre:" + req.body.email);
+    console.log("email:" + req.body.email);
+    console.log("pass:" + req.body.password);
+    res.render("init");
 });
 
 //listen final
