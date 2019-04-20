6afc1f875b9c7003ab4d5a34e24c98faf7f41b54
nico <macharenicolas@gmail.com>
2019-04-20 20:13:57 -0300

    con endpoints

--- app.js
+++ app.js
@@ -115,6 +115,139 @@ app.delete("/usersBorrados", function(req, res){
 
 });
 
+//endopoints
+app.get("/users", function(req, res){
+
+    User.findOne({email: req.body.email,password: req.body.password},function(err, userDoc){
+
+        if(err){
+            console.log("error");
+            //res.statusCode = 301;
+            //res.setHeader("Content-type","text/plain")
+            //res.write("error");
+            res.status(300).send('error');
+        }else{
+            if( userDoc==null){
+                console.log("no encontrado");
+                //res.statusCode = 404;
+                //res.setHeader("Content-type","text/plain");
+                //res.write("Not found");
+                res.status(404).send('not found');
+            }
+            if(!err & userDoc!=null){
+                console.log("devolviendo");
+                //res.statusCode = 200;
+                //res.setHeader('Content-type','application/json');
+                //res.write(JSON.stringify( userDoc));
+                //res.end;
+                res.status(200).send(JSON.stringify(userDoc));
+            }
+        }
+    })
+
+});
+
+app.post("/users", function(req, res){
+
+    console.log("entre3");
+
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, userDoc){
+
+        if(!err){
+            if( userDoc!=null){
+                console.log("repeat");
+                res.status(401).send('unauthorized');
+            }else{
+                var user = new User({name: req.body.name, email: req.body.email, password: req.body.password});
+
+                user.save().then(function(us){
+                    console.log("entre bien");
+                    res.status(200).send('OK'); 
+                },function(err){
+                    if(err){
+                        console.log("entre por error");
+                        res.status(403).send(String(err)); 
+                    }
+                });
+            } 
+        }
+
+        if(err){
+            console.log("error diferente");
+            res.status(405).send('unexpected error');
+        }
+
+    })
+
+});
+
+app.put("/users", function(req, res){
+
+
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, userDoc){
+
+        if(!err){
+            if( userDoc==null){
+                console.log("not found");
+                res.status(404).send('not found');
+            }else{
+                userDoc.name = req.body.name;
+
+                userDoc.save().then(function(us){
+                    console.log("entre bien");
+                    res.status(200).send('OK'); 
+                },function(err){
+                    if(err){
+                        console.log("entre por error");
+                        res.status(403).send(String(err)); 
+                    }
+                });
+            } 
+        }
+
+        if(err){
+            console.log("error diferente");
+            res.status(405).send('unexpected error');
+        }
+
+    })
+
+});
+
+app.delete("/users", function(req, res){
+
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, userDoc){
+
+        if(!err){
+            if( userDoc==null){
+                console.log("not found");
+                res.status(404).send('not found');
+            }else{
+
+                userDoc.remove(function(err){
+            
+                    if(!err){
+                        console.log("eliminando");
+                        res.status(200).send('ok');
+                    }else{
+                        console.log("error diferente");
+                        res.status(405).send('unexpected error');
+                    }
+
+                });
+            } 
+        }
+
+        if(err){
+            console.log("error diferente");
+            res.status(405).send('unexpected error');
+        }
+
+    })
+
+});
+
+
 
 
 //listen final
