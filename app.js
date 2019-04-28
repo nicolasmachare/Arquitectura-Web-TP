8f7901ce73a6e9ff3fa3d237400711a23d2c5824
nico <macharenicolas@gmail.com>
2019-04-28 06:24:54 -0300

    Con cuentas 2.1

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var User = require("./models/users").User;
 var session = require("express-session");
 var methodOverride = require("method-override");
+
+var User = require("./models/users").User;
+var Account = require("./models/accounts").Account;
 
 app.use(session({
     secret: "qwer",
@@ -24,7 +26,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/test", function(req, res){
-    User.find(function(err,doc){
+    Account.find(function(err,doc){
         console.log(doc)
     });
     res.render("test");
@@ -249,6 +251,178 @@ app.delete("/users", function(req, res){
                     }
 
                 });
+            } 
+        }
+
+        if(err){
+            console.log("error diferente");
+            res.status(405).send('unexpected error');
+        }
+
+    })
+
+});
+
+//cuentas
+
+app.get("/accounts", function(req, res){
+
+    Account.findOne({email: req.body.email, id: req.body.id},function(err, accountDoc){
+
+        if(err){
+            console.log("error");
+            res.status(300).send('error');
+        }else{
+            if( accountDoc==null){
+                console.log("no encontrado");
+                res.status(404).send('not found');
+            }
+            if(!err & accountDoc!=null){
+                console.log("devolviendo");
+                res.status(200).send(JSON.stringify(accountDoc));
+            }
+        }
+    })
+
+});
+
+app.post("/accounts", function(req, res){
+
+    console.log("entre3");
+    var emailReq = req.body.email;
+    var passReq = req.body.password;
+    var aliasReq = req.body.alias;
+    var saldoReq = req.body.saldo;
+    var idReq = req.body.id;
+
+    User.findOne({email: emailReq, password: passReq}, function(err, userDoc){
+
+        if(!err){
+            if( userDoc!=null){
+                
+                Account.findOne({email: emailReq, id: idReq}, function(err, accountDoc){
+
+                    if(!err){
+                        if( accountDoc!=null){
+                            console.log("repeat");
+                            res.status(401).send('unauthorized');
+                        }else{
+                            var account = new Account({email: emailReq, id: idReq, saldo: saldoReq, alias: aliasReq});
+            
+                            account.save().then(function(us){
+                                console.log("entre bien");
+                                res.status(200).send('OK'); 
+                            },function(err){
+                                if(err){
+                                    console.log("entre por error");
+                                    res.status(403).send(String(err)); 
+                                }
+                            });
+                        } 
+                    }
+            
+                    if(err){
+                        console.log("error diferente");
+                        res.status(405).send('unexpected error');
+                    }
+            
+                })
+
+            }else{
+                res.status(404).send('not found');
+            } 
+        }
+
+        if(err){
+            console.log("error diferente");
+            res.status(405).send('unexpected error');
+        }
+
+    })
+
+});
+
+
+app.put("/accounts", function(req, res){
+
+    Account.findOne({email: req.body.email, id: req.body.id}, function(err, accountDoc){
+
+        if(!err){
+            if( accountDoc==null){
+                console.log("not found");
+                res.status(404).send('not found');
+            }else{
+                var aliasReq = req.body.alias;
+                var saldoReq = req.body.saldo;
+
+                if(aliasReq!=null){
+                    
+                    accountDoc.alias = aliasReq;
+
+                    accountDoc.save().then(function(us){
+                        console.log("entre bien");
+                        res.status(200).send('OK'); 
+                    },function(err){
+                        if(err){
+                            console.log("entre por error");
+                            res.status(403).send(String(err)); 
+                        }
+                    });
+                }
+
+                if(saldoReq!=null){
+                    
+                    accountDoc.saldo = saldoReq;
+
+                    accountDoc.save().then(function(us){
+                        console.log("entre bien");
+                        res.status(200).send('OK'); 
+                    },function(err){
+                        if(err){
+                            console.log("entre por error");
+                            res.status(403).send(String(err)); 
+                        }
+                    });
+                }
+                
+            } 
+        }
+
+        if(err){
+            console.log("error diferente");
+            res.status(405).send('unexpected error');
+        }
+
+    })
+
+});
+
+app.delete("/accounts", function(req, res){
+
+    Account.findOne({email: req.body.email, id: req.body.id}, function(err, accountDoc){
+
+        if(!err){
+            if( accountDoc==null){
+                console.log("not found");
+                res.status(404).send('not found');
+            }else{
+
+                if(accountDoc.saldo == 0){
+                    accountDoc.remove(function(err){
+            
+                        if(!err){
+                            console.log("eliminando");
+                            res.status(200).send('ok');
+                        }else{
+                            console.log("error diferente");
+                            res.status(405).send('unexpected error');
+                        }
+    
+                    });
+                }else{
+                    res.status(403).send('forbbiden');    
+                }
+                
             } 
         }
 
--- models/accounts.js
+++ models/accounts.js
@@ -4,9 +4,10 @@ var Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost/prueba");
 
 var accountSchemaJson = {
+    id:{type: String, required: "el id es obligatorio"},
     email: {type: String, required: "el email es obligatorio"},
     alias: {type: String, required: "el nombre es obligatorio"},
-    saldo: {type: Float32Array, required: "El monto es obligatorio"}
+    saldo: {type: Number, required: "El monto es obligatorio"}
 };
 
 var accountSchema = new Schema(accountSchemaJson);
